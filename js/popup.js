cf52a874b9a755cc33d4e7d0ae324a0d0cf33fc9
k2Forest-Man <LesnoiChelovek@yandex.ru>
2022-05-20 19:10:10 +0300

    изменил название скрипта модалки, дописал скрипт в модалке, добавил localStorage, закрытие модалки кнопкой ESC

---
+++ js/popup.js
@@ -0,0 +1,53 @@
+const buttonOpenPopup = document.querySelector ('.button-open-popup');
+const closePopup = document.querySelector ('.close-button-popup');
+const popup = document.querySelector ('.popup');
+const form = popup.querySelector(".popup-form");
+const inputName = popup.querySelector('.input-name');
+const inputEmail = popup.querySelector(".input-email");
+const textLetter = popup.querySelector('[name=text-letter]');
+
+let isStorageSupport = true;
+let storage = "";
+
+try {
+  storage = localStorage.getItem("yourName");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+
+buttonOpenPopup.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  popup.classList.add('popup-show');
+
+  if (storage) {
+    inputName.value = storage;
+    inputEmail.focus();
+  } else {
+    inputName.focus();
+  }
+});
+
+closePopup.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  popup.classList.remove('popup-show');
+});
+
+form.addEventListener('submit', (evt) => {
+  if(!inputName.value || !inputEmail.value || !textLetter.value) {
+    evt.preventDefault();
+  } else {
+    if(isStorageSupport) {
+      localStorage.setItem('yourName', inputName.value);
+    }
+  }
+})
+
+window.addEventListener ('keydown', (evt) => {
+  if(evt.keyCode === 27) {
+    if(popup.classList.contains('popup-show')) {
+      evt.preventDefault();
+      popup.classList.remove('popup-show');
+    }
+  }
+})
